7650e0d67154aa12c99fe481cebde19a20df4a03
Ars Vladimirov <grokhael@gmail.com>
2023-06-15 10:18:24 +0400

    init convert

--- src/convert.py
+++ src/convert.py
@@ -1,15 +1,98 @@
-# Path to the original dataset
+# https://zenodo.org/record/7781388
 
+import os
+import xml.etree.ElementTree as ET
+
+import numpy as np
 import supervisely as sly
+from dotenv import load_dotenv
+from supervisely.io.fs import (
+    file_exists,
+    get_file_ext,
+    get_file_name,
+    get_file_name_with_ext,
+    get_file_size,
+)
+
+# if sly.is_development():
+load_dotenv("local.env")
+load_dotenv(os.path.expanduser("~/supervisely.env"))
+
+api = sly.Api.from_env()
+team_id = sly.env.team_id()
+workspace_id = sly.env.workspace_id()
+
+
+project_name = "Aerial Power Infrastructure"
+dataset_paths = ["./APP_DATA/Aerial/Train", "./APP_DATA/Aerial/Test", "./APP_DATA/Aerial/Valid"]
+anns_paths = [
+    "./APP_DATA/Aerial/Annotations/VOC/Train",
+    "./APP_DATA/Aerial/Annotations/VOC/Test",
+    "./APP_DATA/Aerial/Annotations/VOC/Valid",
+]
+batch_size = 10
+ds_names = ["Train", "Test", "Valid"]
+images_ext = ".jpg"
+ann_ext = ".xml"
+
+
+def create_ann(image_path, anns_path):
+    labels = []
+
+    image_np = sly.imaging.image.read(image_path)[:, :, 0]
+    img_height = image_np.shape[0]
+    img_wight = image_np.shape[1]
+
+    file_name = get_file_name(image_path)
+
+    ann_path = os.path.join(anns_path, file_name + ann_ext)
+
+    if file_exists(ann_path):
+        tree = ET.parse(ann_path)
+        root = tree.getroot()
+
+        coords_xml = root.findall(".//bndbox")
+        for curr_coord in coords_xml:
+            left = int(curr_coord[0].text)
+            top = int(curr_coord[1].text)
+            right = int(curr_coord[2].text)
+            bottom = int(curr_coord[3].text)
+
+            rect = sly.Rectangle(left=left, top=top, right=right, bottom=bottom)
+            label = sly.Label(rect, obj_class)
+            labels.append(label)
+
+    return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
+
+
+obj_class = sly.ObjClass("tbar", sly.Rectangle)
 
 
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    # * dataset_path = "Path to the local dir with dataset"
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(obj_classes=[obj_class])
+    api.project.update_meta(project.id, meta.to_json())
 
-    # Function should read local dataset and upload it to Supervisely project, then return project info.
+    for ds_name, dataset_path, anns_path in zip(ds_names, dataset_paths, anns_paths):
+        images_names = os.listdir(dataset_path)
 
-    raise NotImplementedError("The converter should be implemented manually.")
+        dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
 
-    # * return project
+        progress = sly.Progress("Create dataset {}".format(ds_name), len(images_names))
+
+        for images_names_batch in sly.batched(images_names, batch_size=batch_size):
+            img_pathes_batch = [
+                os.path.join(dataset_path, image_name) for image_name in images_names_batch
+            ]
+
+            img_infos = api.image.upload_paths(dataset.id, images_names_batch, img_pathes_batch)
+            img_ids = [im_info.id for im_info in img_infos]
+
+            anns = [create_ann(image_path, anns_path) for image_path in img_pathes_batch]
+            api.annotation.upload_anns(img_ids, anns)
+
+            progress.iters_done_report(len(images_names_batch))
+
+    return project
--- src/settings.py
+++ src/settings.py
@@ -5,40 +5,48 @@ from dataset_tools.templates import AnnotationType, CVTask, Industry, License
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "Aerial Power"
+PROJECT_NAME_FULL: str = "Aerial Power Infrastructure Detection Dataset"
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-INDUSTRIES: List[Industry] = None
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+LICENSE: License = License.CC_BY_4_0()
+INDUSTRIES: List[Industry] = [Industry.Energy()]
+CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
-RELEASE_YEAR: int = None
-HOMEPAGE_URL: str = None
+RELEASE_YEAR: int = 2023
+HOMEPAGE_URL: str = "https://zenodo.org/record/7781388"
 # e.g. "https://some.com/dataset/homepage"
 
 PREVIEW_IMAGE_ID: int = None
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/aerial-power"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = {
+    "Annotations": "https://zenodo.org/record/7781388/files/Annotations.zip?download=1",
+    "Labels": "https://zenodo.org/record/7781388/files/labels.txt?download=1",
+    "Test": "https://zenodo.org/record/7781388/files/Test.zip?download=1",
+    "Train": "https://zenodo.org/record/7781388/files/Train.zip?download=1",
+    "Valid": "https://zenodo.org/record/7781388/files/Valid.zip?download=1",
+}
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
-PAPER: Optional[str] = None
-CITATION_URL: Optional[str] = None
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+PAPER: Optional[str] = "https://ieeexplore.ieee.org/document/9476742/"
+CITATION_URL: Optional[str] = "https://zenodo.org/record/7781388"
+ORGANIZATION_NAME: Optional[
+    Union[str, List[str]]
+] = "KIOS Research and Innovation Center of Excellence, University of Cyprus"
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = "https://www.kios.ucy.ac.cy/"
 TAGS: List[str] = None
 
 ##################################
