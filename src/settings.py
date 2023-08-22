baf2c1df670dd80a266e83772dfa89f290ea60ba
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-23 00:11:13 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -15,6 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Aerial Power Infrastructure"
 PROJECT_NAME_FULL: str = "Aerial Power Infrastructure Detection Dataset"
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
@@ -90,6 +91,7 @@ def get_settings():
     settings = {
         "project_name": PROJECT_NAME,
         "license": LICENSE,
+        "hide_dataset": HIDE_DATASET,        
         "applications": APPLICATIONS,
         "category": CATEGORY,
         "cv_tasks": CV_TASKS,
